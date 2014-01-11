dc0b17034b5a858ab8a295e2740e02361784b08f
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-11 13:23:47 -0600

    Adds Bootstrap.CSS & Bootstrap.JS

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module Fanfollow
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,4 +26,7 @@ module Fanfollow
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
+
+
 end
