edafa15698c8e288d5518bee9c05220495ef84b4
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-11 13:03:13 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fanfollow::Application.config.secret_key_base = '33295b1bc556e5ebd5bf62d1a47694b2d212c645f66d23ed7356117a88ad7b39a28ec7e6ad4260104cac5c842db04254ec214f1ff2dbe547ce7a8b195abe8db7'
