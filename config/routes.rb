4eeb186a13488c97befc1c4c67b522694423f0a7
Andy Murphy <mrajmurphy@gmail.com>
2014-01-11 17:44:34 -0600

    Fix logout / DELETE links

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Fanfollow::Application.routes.draw do
   root 'home#landing_page'
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
