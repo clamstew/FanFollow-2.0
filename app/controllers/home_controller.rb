812eeacd94ea8b3b474922e7098db13fa71bf243
Andy Murphy <mrajmurphy@gmail.com>
2014-01-12 02:50:51 -0600

    Event table interaction, hash to json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ActionController::Base
   def landing_page
+  	@events = Event.all
+  	@events_search = Hash.new
+  	@events.each do |event|
+  		id = event.id
+  		@events_search[id] = event.title
+  	end
     render :layout => 'application'
   end
 end
