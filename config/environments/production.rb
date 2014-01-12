aafcfdd0ff0a6825ad84749669fc93eba3a3f419
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-11 21:22:50 -0600

    Changes to Production.RB for Heroku, 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,6 @@ Fanfollow::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
-
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -38,8 +36,8 @@ Fanfollow::Application.configure do
   config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+   #config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
