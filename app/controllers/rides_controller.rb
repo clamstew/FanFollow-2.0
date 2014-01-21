362ac142520878560b07e927b3654dbea0cfe6fc
gpfeff <greg.a.pfeffer@gmail.com>
2014-01-20 18:05:11 -0600

    Create New Ride Form, **NOT WORKING**

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,7 @@ class RidesController < ApplicationController
   def show
   	@params = params
   	@rides = Ride.all
-  	@ride = Ride.find(params[:ride_id])
+  	@ride = Ride.find(params[:id])
   	@title = @ride.title
   	@local_start = @ride.departure_time_local
   	@max_seats = @ride.max_seats
@@ -16,5 +16,31 @@ class RidesController < ApplicationController
   	@user = User.all
     render :layout => 'application'
   end
+  
+  def new
+    @event_id = params[:event_id]
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.new(ride_params)
+    if @ride.save
+      redirect_to @ride
+    end
+  end
+
+
+  def edit
+    @ride = Ride.find(params[:id])
+  end
+
+  protected
+  def ride_params
+    params.require(:ride).permit(:title, :origin, :max_seats, :price_per_seat, :image, :user_id)
+  end
+
+
+
+
 
 end
