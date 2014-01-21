086a5c5e50b2219046d7a4332effd46e90f59991
Andy Murphy <mrajmurphy@gmail.com>
2014-01-21 13:20:20 -0600

    Add pry gems

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,12 +7,22 @@ class RidesController < ApplicationController
   end
   
   def new
-    @event = Event.find(params[:event_id])
-    @ride = @event.rides.new(user_id: current_user)
+    event = Event.find(params[:event_id])
+    setup_new_ride(current_user, event)
+    # 
   end
 
   def create
-    @ride = Ride.new(ride_params)
+    # @ride = Ride.new(ride_params)
+    @ride
+    @ride.title = ride_params[:title]
+    @ride.origin = ride_params[:origin]
+    @ride.max_seats = ride_params[:max_seats]
+    @ride.price_per_seat = ride_params[:price_per_seat]
+    @ride.image = ride_params[:image]
+    # @ride.user_id = ride_params[:user_id]
+    # @ride.event_id = ride_params[:event_id]
+    @ride.departure_time_local = ride_params[:departure_time_local]
 
     if @ride.save
       redirect_to root_path
