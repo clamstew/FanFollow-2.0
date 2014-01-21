dda8f04f761c312114d760ef2637043d6399d255
Andy Murphy <mrajmurphy@gmail.com>
2014-01-20 22:40:16 -0600

    Begin fixing model associations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,10 @@
 class Ride < ActiveRecord::Base
-  belongs_to :event
-  belongs_to :user
-  belongs_to :driver, class_name:"User", foreign_key:"user_id"
-  has_many :seats
-  has_many :passengers, :through => :seats, :source =>:user
+    belongs_to :event
+
+    belongs_to :driver, class_name: "User", foreign_key: "user_id"
+    has_many :seats
+		has_many :passengers, :through => :seats, :source => :user
+	def attendees
+    User.where(id: (seats.select(:user_id).map(&:user_id) << user_id))
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :rides
-  has_many :events, through: :rides
+
+  has_many :ride_offers, class_name: "Ride", foreign_key: "user_id"
+  has_many :events, through: :ride_offers
   has_many :seats
   has_many :rides, through: :seats
 
@@ -10,3 +11,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+# http://guides.rubyonrails.org/association_basics.html#has-many-association-reference
+# look for class_name, foreign_key and source options
