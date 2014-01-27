96e9c32d4ddd4bd9ac61f671201998b7c76d110b
Andy Murphy <mrajmurphy@gmail.com>
2014-01-27 02:32:39 -0600

    Clean up event controller and show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,10 @@
 class EventsController < ApplicationController
   def show
   	@params = params
-    @events = Event.all
+    # @events = Event.all
   	@event = Event.find(params[:event_id]);
-    @title = @event.title
-    @venue = @event.venue
-    @category = @event.category
-    @city = @event.city
-    @state = @event.state
-    @country = @event.country
-    @url = @event.url
-    @tickets = @event.url
-    @local_start = @event.start_datetime_local
-    @local_end = @event.end_datetime_local
-    @UTC_start = @event.start_datetime_utc
-    @UTC_end = @event.end_datetime_utc
-
-    # Rides to display
     @rides = @event.rides
 
-    render :layout => 'application'
   end
 
   def show_all
